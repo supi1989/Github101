3b7f85dad06b8dd85a3e319ad2fc4fe5237b3c55
supi1989 <supisara.po@kkumail.com>
2023-08-15 04:01:55 +0700

    Create user.php

---
+++ user.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis laboriosam accusantium alias iusto dolor quaerat blanditiis deleniti! Quis officia labore maiores id incidunt itaque, dolorem voluptatum, doloremque nisi accusantium vel!
+    </p>
+</body>
+</html>
